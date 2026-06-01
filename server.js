88cdc44dc4a9ef35bd9c5455fdb05c4174384e7a
Dhivagarsoar <soar.developer1@gmail.com>
2026-06-01 18:05:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,12 @@ app.use(
   cors({
     origin: true,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options("*", cors());
 // app.use(
 //   cors({
 //     origin: [
